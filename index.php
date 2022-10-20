c59c1aeb4e1e596851447c6b678b7c096ca12387
Prof. Chico Freitas <chicoo.freitas84@gmail.com>
2022-10-20 18:45:50 -0300

    Working with redirects

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-//
 //var_dump( getenv('PHP_ENV'), $_SERVER, $_REQUEST);
 
 /**
@@ -15,11 +14,24 @@ $requestMethod = $_SERVER['REQUEST_METHOD'];
 $requestPath = $_SERVER['REQUEST_URI'];
 
 /**
- *
- *  
+ * 
+ * 
+ * @param $requestMethod string
+ * @param $requestPath string
+ * @return Heredoc|string
  */
 if ($requestMethod === 'GET' and $requestPath === '/') {
-    print 'Hello, World!';
+    print <<<HTML
+        <!Doctype html>
+        <html lang="en">
+            <body>
+                hello man!!
+            </body>
+        </html>
+    HTML;
+}else if($requestPath === '/home'){
+    header('Location: /', $replace=true, $code = 301);
+    exit;
 }else{
-    print '404 Not Found.';
+    include(__DIR__.'/includes/404.php');
 }
