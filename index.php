90e222638c293e7be32f3046fa1c899aa900bd58
Prof. Chico Freitas <chicoo.freitas84@gmail.com>
2022-10-20 18:27:23 -0300

    Analyzing $_SERVER variable and 'print' defaults

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
+//
+//var_dump( getenv('PHP_ENV'), $_SERVER, $_REQUEST);
 
-phpinfo();
+/**
+ * 
+ * @var string
+ */
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+/**
+ * 
+ * @var string
+ */
+$requestPath = $_SERVER['REQUEST_URI'];
+
+/**
+ *
+ *  
+ */
+if ($requestMethod === 'GET' and $requestPath === '/') {
+    print 'Hello, World!';
+}else{
+    print '404 Not Found.';
+}
